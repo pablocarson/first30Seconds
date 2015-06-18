f7bcbd95292c824218a8d2add85eac77927a3c1a
Paul Caron <branduxnyc@gmail.com>
2015-06-18 16:33:39 -0400

    testing the atParty page with streamlined edge case

--- js/unit_test.js
+++ js/unit_test.js
@@ -1,25 +1,8 @@
 function unitTests() {
     var allTests = [
-        //  Step-by-step walkthrough of the unit test.
+        // Walkthrough notes are displayed in the console (no pauses)
             function() {
-                descAlert = function() {};
-                var alertstop = confirm("Click 'OK' to show walkthrough text as alerts on the phone  / Click 'Cancel' to show walkthrough text in the Javascript console.");
-                if (alertstop == true)
-                    // {}
-                    descAlert = function( txt ) { window.alert (txt) };
-                else {
-                    // descAlert = function() {};
-                    descAlert = function( txt ) { console.log (txt); alert ("click to continue"); }
-                };
-            },
-            function () {
-                // Check whether Cordova is active
-                    platform = (window.cordova === undefined);
-                // If it's not, this means it's running on a desktop or other non-Android device, so notify the user that certain tests that require the test to be run on an actual phone will be skipped.
-                    if (platform == true) {
-                        alert ("Phonegap is not running. This may be because the unit test is being run on a desktop. The following tests will be skipped: Tests for Android version, Phonegap device.uuid, Google Cloud Messaging (GCM) registration ID and geolocation test.")
-                        return (0);
-                    };
+                descAlert = function( txt ) { console.log (txt); }
             },
         // P7. atParty page
             // change display to atParty page
